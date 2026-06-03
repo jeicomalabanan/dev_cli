521b811dacbb4c40deed044b3fbcf28aac5f1c17
jeicofst <107014518+jeicofst@users.noreply.github.com>
2026-06-03 21:49:39 +0800

    chore: wip

--- lib/src/commands/create/create_package_command.dart
+++ lib/src/commands/create/create_package_command.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:args/command_runner.dart';
 import 'package:mason/mason.dart';
 
@@ -11,4 +13,11 @@ final class CreatePackageCommand extends Command<void> {
 
   @override
   String get description => 'Create a new package';
+
+  @override
+  FutureOr<void>? run() async {
+    final progress = logger.progress('Creating app...');
+    await Future.delayed(const Duration(milliseconds: 2000));
+    progress.complete('App created.');
+  }
 }
