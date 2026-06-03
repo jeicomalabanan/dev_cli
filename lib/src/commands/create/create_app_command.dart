972d4f2f77da86562552f32cf0a97d0c39c3b441
jeicofst <107014518+jeicofst@users.noreply.github.com>
2026-06-03 21:25:17 +0800

    chore: update create app

--- lib/src/commands/create/create_app_command.dart
+++ lib/src/commands/create/create_app_command.dart
@@ -5,17 +5,10 @@ import 'package:args/command_runner.dart';
 import 'package:mason/mason.dart';
 import 'package:path/path.dart' as path;
 
-import '../../../bundles/app_bundle.dart';
 import '../../extensions/logger_extensions.dart';
 import '../../models/enums/app_platform.dart';
 import '../../models/enums/app_template.dart';
 import '../../utils/process_runner.dart';
-import '../upgrade/enum_extensions.dart';
-
-const _argKeyTemplate = 'template';
-const _argKeyName = 'name';
-const _argKeyOrg = 'org';
-const _argKeyPlatforms = 'platforms';
 
 final class _Args {
   const _Args({
@@ -32,36 +25,7 @@ final class _Args {
 }
 
 final class CreateAppCommand extends Command<void> {
-  CreateAppCommand(this.logger) {
-    argParser
-      ..addOption(
-        _argKeyTemplate,
-        help: 'Specify the template of the application to create.',
-        allowed: AppTemplate.values.map((e) => e.name).toList(),
-        allowedHelp: {
-          for (final template in AppTemplate.values)
-            template.name: template.description,
-        },
-      )
-      ..addOption(
-        _argKeyName,
-        help: 'The app name for this new Flutter application.',
-      )
-      ..addOption(
-        _argKeyOrg,
-        help:
-            'The organization responsible for the new Flutter application, in reverse domain name notation (e.g. com.example).',
-      )
-      ..addOption(
-        _argKeyPlatforms,
-        help: 'The platforms supported by this application.',
-        allowed: AppPlatform.values.map((e) => e.name).toList(),
-        allowedHelp: {
-          for (final platform in AppPlatform.values)
-            platform.name: platform.description,
-        },
-      );
-  }
+  CreateAppCommand(this.logger) {}
 
   final Logger logger;
 
@@ -76,87 +40,37 @@ final class CreateAppCommand extends Command<void> {
     final args = _promptArgs();
     if (args == null) return;
 
-    final currentDir = Directory.current.path;
-    final appsDir = path.join(currentDir, 'apps');
-    final appDir = path.join(appsDir, args.name);
-
-    // check if app already exists
-    if (Directory(appDir).existsSync()) {
-      logger.err('"${args.name}" already exists at $appDir');
-      return;
-    }
-
     switch (args.template) {
       case AppTemplate.basic:
-        _generateBasicApp();
+        _generateBasicApp(currentDir: Directory.current.path, args: args);
         break;
       case AppTemplate.monorepo:
         _generateMonorepoApp();
         break;
     }
 
-    // create flutter application
-    final result = ProcessRunner.createFlutterApp(
-      name: args.name,
-      org: args.org,
-      platforms: args.platforms.map((e) => e.name).join(','),
-      targetDirectory: appsDir,
-    );
-    if (result.exitCode != 0) {
-      logger.err(result.stderr);
-      return;
-    }
-
-    // final pathsToDelete = ['$targetDir/test', '$targetDir/pubspec.yaml'];
-    // await FileUtil.deletePaths(pathsToDelete);
-
-    // generate app from mason bricks
-    final generator = await MasonGenerator.fromBundle(appBundle);
-    await generator.generate(
-      DirectoryGeneratorTarget(Directory(appDir)),
-      vars: {_argKeyName: args.name},
-      fileConflictResolution: FileConflictResolution.overwrite,
-    );
-
     logger.success('✅ "${args.name}" created successfully.');
   }
 
   _Args? _promptArgs() {
-    final templateValue = argResults?[_argKeyTemplate] as String?;
-    final template =
-        AppTemplate.values.byNameOrNull(templateValue ?? '') ??
-        logger.chooseOneEnum(
-          message: 'Choose a template:',
-          values: AppTemplate.values.toList(),
-          defaultValue: AppTemplate.monorepo,
-        );
+    final template = logger.chooseOneEnum(
+      message: 'Choose template:',
+      values: AppTemplate.values.toList(),
+      defaultValue: AppTemplate.monorepo,
+    );
 
-    final name =
-        argResults?[_argKeyName] as String? ??
-        logger.prompt('Name of the app:', defaultValue: 'user_app');
+    final name = logger.prompt('Name of the app:', defaultValue: 'user_app');
 
-    final org =
-        argResults?[_argKeyOrg] as String? ??
-        logger.prompt('Organization:', defaultValue: 'team.workspace');
+    final org = logger.prompt('Organization:', defaultValue: 'team.workspace');
 
-    final platformValues = (argResults?[_argKeyPlatforms] as String? ?? '')
-        .split(',')
-        .map((name) => AppPlatform.values.byNameOrNull(name.trim()))
-        .nonNulls
-        .toList();
-    final platforms = platformValues.isNotEmpty
-        ? platformValues
-        : logger.chooseAnyEnum(
-            message: 'Choose supported platforms:',
-            values: AppPlatform.values,
-            defaultValues: [
-              AppPlatform.android,
-              AppPlatform.ios,
-              AppPlatform.web,
-            ],
-          );
+    final platforms = logger.chooseAnyEnum(
+      message: 'Choose platforms:',
+      values: AppPlatform.values.toList(),
+      defaultValues: [AppPlatform.android, AppPlatform.ios, AppPlatform.web],
+    );
 
     logger.info('🚀 Creating an app...');
+    logger.detail('Template     : ${template.name}');
     logger.detail('Name         : $name');
     logger.detail('Organization : $org');
     logger.detail('Platforms    : ${platforms.map((e) => e.name).join(',')}');
@@ -169,7 +83,49 @@ final class CreateAppCommand extends Command<void> {
     );
   }
 
-  void _generateBasicApp() {}
+  void _generateBasicApp({required String currentDir, required _Args args}) {
+    final appDir = path.join(currentDir, args.name);
 
-  void _generateMonorepoApp() {}
+    // check if app already exists
+    if (Directory(appDir).existsSync()) {
+      logger.err('"${args.name}" already exists at $appDir');
+      return;
+    }
+
+    ProcessRunner.run(
+      command: 'flutter',
+      args: [
+        'create',
+        '--template=app',
+        args.name,
+        '--org=${args.org}',
+        '--platforms=${args.platforms}',
+      ],
+    );
+  }
+
+  void _generateMonorepoApp() {
+    // // create flutter application
+    // final result = ProcessRunner.createFlutterApp(
+    //   name: args.name,
+    //   org: args.org,
+    //   platforms: args.platforms.map((e) => e.name).join(','),
+    //   targetDirectory: appsDir,
+    // );
+    // if (result.exitCode != 0) {
+    //   logger.err(result.stderr);
+    //   return;
+    // }
+
+    // // final pathsToDelete = ['$targetDir/test', '$targetDir/pubspec.yaml'];
+    // // await FileUtil.deletePaths(pathsToDelete);
+    //
+    // // generate app from mason bricks
+    // final generator = await MasonGenerator.fromBundle(appBundle);
+    // await generator.generate(
+    //   DirectoryGeneratorTarget(Directory(appDir)),
+    //   vars: {_argKeyName: args.name},
+    //   fileConflictResolution: FileConflictResolution.overwrite,
+    // );
+  }
 }
