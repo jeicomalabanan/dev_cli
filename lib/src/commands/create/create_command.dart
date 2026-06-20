bb4d88dd64e961c6afc7154d83d595c9b26ecc78
jeicofst <107014518+jeicofst@users.noreply.github.com>
2026-06-20 13:07:18 +0800

    chore: update monorepo

--- lib/src/commands/create/create_command.dart
+++ lib/src/commands/create/create_command.dart
@@ -2,9 +2,11 @@ import 'package:args/command_runner.dart';
 import 'package:mason/mason.dart';
 
 import 'create_feature_command.dart';
+import 'create_monorepo_command.dart';
 
 final class CreateCommand extends Command<void> {
   CreateCommand(this.logger) {
+    addSubcommand(CreateMonorepoCommand(logger));
     addSubcommand(CreateFeatureCommand(logger));
   }
 
