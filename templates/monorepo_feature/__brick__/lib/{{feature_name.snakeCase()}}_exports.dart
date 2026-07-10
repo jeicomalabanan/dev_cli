27e5ba6cca6a2560197b493f198287222b0e7a00
jeicofst <107014518+jeicofst@users.noreply.github.com>
2026-07-11 02:46:38 +0800

    chore: wip

--- templates/monorepo_feature/__brick__/lib/{{feature_name.snakeCase()}}_exports.dart
+++ templates/monorepo_feature/__brick__/lib/{{feature_name.snakeCase()}}_exports.dart
@@ -1,3 +1,6 @@
 library;
 
+export 'src/contracts/{{feature_name.snakeCase()}}_data_contract.dart';
+export 'src/contracts/{{feature_name.snakeCase()}}_ui_contract.dart';
 export 'src/di/di.module.dart';
+export 'src/domain/repositories/{{feature_name.snakeCase()}}_repository.dart';
