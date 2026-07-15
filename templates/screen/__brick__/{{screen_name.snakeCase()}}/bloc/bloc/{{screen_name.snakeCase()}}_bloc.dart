d153d9c12824b8d46d21fa2bc16e83caa4a75278
jeicofst <107014518+jeicofst@users.noreply.github.com>
2026-07-15 23:05:39 +0800

    chore: wip

--- templates/screen/__brick__/{{screen_name.snakeCase()}}/bloc/bloc/{{screen_name.snakeCase()}}_bloc.dart
+++ templates/screen/__brick__/{{screen_name.snakeCase()}}/bloc/bloc/{{screen_name.snakeCase()}}_bloc.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:core_architecture/core_architecture.dart';
+import 'package:injectable/injectable.dart';
 
 part '{{screen_name.snakeCase()}}_event.dart';
 part '{{screen_name.snakeCase()}}_state.dart';
 
+@injectable
 final class {{screen_name.pascalCase()}}Bloc extends BaseBloc<{{screen_name.pascalCase()}}Event, {{screen_name.pascalCase()}}State> {
   {{screen_name.pascalCase()}}Bloc() : super(const {{screen_name.pascalCase()}}State()) {
     on<{{screen_name.pascalCase()}}Started>(_on{{screen_name.pascalCase()}}Started);
